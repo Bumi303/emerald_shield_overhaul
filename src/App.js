419e03075ba6da4d9a948ee974d8afcd03aaf946
Bumi303 <hayden.henderson03@gmail.com>
2023-11-25 17:52:45 -0800

    1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// src/App.js
+
+import React from 'react';
+import StockTable from './components/StockTable';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Emerald Shield</h1>
+            <StockTable />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/StockTable.js
@@ -0,0 +1,24 @@
+// StockTable.js
+
+const StockTable = () => {
+    return (
+        <div>
+            <h1>Stock Market Information</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Open</th>
+                        <th>High</th>
+                        <th>Low</th>
+                        <th>Close</th>
+                        {/* Add more columns as needed */}
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+        </div>
+    );
+};
+
+export default StockTable;
